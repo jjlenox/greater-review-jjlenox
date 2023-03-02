80318d2d24dced89adaef76eb383434c6b8d9d15
jjlenox <joshua.watersjr@gmail.com>
2023-03-02 12:10:10 -0800

    g
    g

--- grade.sh
+++ grade.sh
@@ -3,3 +3,16 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+cd student-submission
+if [[ -f ListExamples.java ]]
+then
+    echo "java files found"
+else
+    echo "need a java file"
+    exit 1
+fi
+cp ../TestListExamples.java ./
+
+javac -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar *.java
+
+java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
