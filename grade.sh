1b6460ecd69557abcf8730809ad237e9dbe5e567
jjlenox <joshua.watersjr@gmail.com>
2023-03-02 13:13:56 -0800

    commit

--- grade.sh
+++ grade.sh
@@ -11,8 +11,26 @@ else
     echo "need a java file"
     exit 1
 fi
+
+javac ListExamples.java
+if [ $? -ne 0 ]
+then 
+    echo "compile error"
+    exit 1
+fi
+
+
 cp ../TestListExamples.java ./
+
 
 javac -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar *.java
 
-java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > test.txt
+
+grep "Failures" test.txt > fail.txt
+if [ -s fail.txt ]
+then
+    echo "failed F---- 0/10 tu es mal oú tête"
+else
+    echo "passed A++++ 10/10ign"
+fi
